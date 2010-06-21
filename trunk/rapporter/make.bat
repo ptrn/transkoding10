8858d201628805cda6ab0ca29061e5620d7fd89f
pettern800@gmail.com <pettern800@gmail.com@70cbe4ca-aede-f5ee-a09d-6722f6e61432>
2010-06-21 21:14:03 +0000

    flyttet på filer

--- trunk/rapporter/make.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\Folders\SVN\sommerjobb '10\rapporter"
-"C:\Program Files (x86)\MiKTeX 2.8\miktex\bin\pdflatex.exe" kartlegging.tex
-"C:\Program Files (x86)\Adobe\Acrobat 9.0\Acrobat\Acrobat.exe" kartlegging.pdf
-exit
